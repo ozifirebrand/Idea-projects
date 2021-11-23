fbeb48560f367a9915ef5b4124084f25fe8b9db3
Ozioma <oziomaokoroafor@gmail.com>
2021-11-23 17:02:31 +0100

    Jijimoji commit 5

--- jijimoji/src/main/java/africa/semicolon/jijimoji/data/repositories/ListerRepository.java
+++ jijimoji/src/main/java/africa/semicolon/jijimoji/data/repositories/ListerRepository.java
@@ -4,6 +4,9 @@ import africa.semicolon.jijimoji.data.models.Lister;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ListerRepository extends MongoRepository<Lister, String> {
+    Optional<Lister> findListerByEmail(String email);
 }
