ba8354b9a0833ad9844d5a1929b156801d561d88
Ozioma <oziomaokoroafor@gmail.com>
2021-11-23 14:54:03 +0100

    Jijimoji commit 3

--- jijimoji/src/main/java/africa/semicolon/jijimoji/data/models/Lister.java
+++ jijimoji/src/main/java/africa/semicolon/jijimoji/data/models/Lister.java
@@ -1,11 +1,14 @@
 package africa.semicolon.jijimoji.data.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Lister {
+    @Id
+    private String id;
     private String fullName;
     private String phoneNumber;
     private String email;
