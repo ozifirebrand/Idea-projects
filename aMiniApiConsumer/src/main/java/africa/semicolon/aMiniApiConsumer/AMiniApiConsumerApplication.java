f1219fb890ebf2deeb7a52eb25c1601de584294a
Ozioma <oziomaokoroafor@gmail.com>
2022-02-24 16:04:35 +0100

    A mini api consumer application

---
+++ aMiniApiConsumer/src/main/java/africa/semicolon/aMiniApiConsumer/AMiniApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.aMiniApiConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMiniApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMiniApiConsumerApplication.class, args);
+	}
+
+}
