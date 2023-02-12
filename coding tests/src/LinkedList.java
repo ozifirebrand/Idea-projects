1b30079b9f0ca7ad5bff94ca013f73fc4a7ace67
OziomaOkoroafor <ozioma@crust.africa>
2023-02-12 23:30:03 +0100

    Implementing LinkedList

---
+++ coding tests/src/LinkedList.java
@@ -0,0 +1,34 @@
+public class LinkedList {
+    private Node node;
+
+    static class Node{
+        int data;
+        Node nextAddress;
+    }
+    public static int push(Node head, int newData){
+        Node node = new Node();
+        node.data = newData;
+        node.nextAddress= head;
+        head= node;
+        return head.data;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node();
+        head.data=5;
+
+        Node secondNode = new Node();
+        secondNode.data=1;
+
+        Node thirdNode = new Node();
+        thirdNode.data= 67;
+
+        head.nextAddress= secondNode;
+        secondNode.nextAddress= thirdNode;
+
+        int newData = 20;
+        System.out.println(push(head, newData));
+    }
+
+
+}
---
+++ printer-frontend/src/components/PrinterFront.vue
@@ -0,0 +1,36 @@
+<template>
+  <input type="text" id="input-quantity" placeholder="Quantity">
+  <div>{{selected}}</div>
+  <select v-model="selected">
+    <option disabled value="">Size</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+  </select>
+  <div id="price">{{priceComputed}}
+  </div>
+
+  <div>
+    <button type="submit">Make Order</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      quantity:0,
+      selected:""
+    }
+  },
+  priceComputed(){
+      return this.selected * this.quantity
+  },
+  name: "PrinterFront"
+}
+</script>
+
+<style scoped>
+
+</style>
